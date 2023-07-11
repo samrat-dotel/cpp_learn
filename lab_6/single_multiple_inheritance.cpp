575a48e6da7b11167c682fd6283ac8b67aa3f8f7
samar_dotel <120027292+samrat-dotel@users.noreply.github.com>
2023-07-11 14:08:27 +0545

    Update single_multiple_inheritance.cpp

--- lab_6/single_multiple_inheritance.cpp
+++ lab_6/single_multiple_inheritance.cpp
@@ -1,75 +1,59 @@
-/* WAP to illustrate the use of constructor in single and multiple inheritance. */
+/* WAP to illustrate the woring of constructor in single and multiple inheritance. */
 
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-class Parent1 {
-private:
-    string name, address;
+class Parent1{
+    private:
+        string name, address;
 
-public:
-    Parent1() {
-        cout << "Enter the name:";
-        getline(cin, name);
-        cout << "Enter address:";
-        getline(cin, address);
+    public:
+    Parent1(){ 
+    cout<<"Enter the name:";
+    getline(cin,name);
+    cout<<"Enter address:";
+    getline(cin,address);
     }
 
-    ~Parent1() {
-        cout << "Parent1 destructor called" << endl;
-    }
-
-    void display() {
-        cout << "Name:" << name << endl << "Address:" << address << endl;
+    void display(){
+        cout<<"Name:"<<name<<endl<<"Address:"<<address<<endl;
     }
 };
 
-class Parent2 {
-private:
-    string rollNo, age;
+class Parent2{
+    private:
+        string rollNo, age;
 
-public:
-    Parent2() {
-        cout << "Enter roll number:";
-        cin >> rollNo;
-        cout << "Enter age:";
-        cin >> age;
+    public:
+    Parent2(){
+        cout<<"Enter roll number:";
+        cin>>rollNo;
+        cout<<"Enter age:";
+        cin>>age;
     }
 
-    ~Parent2() {
-        cout << "Parent2 destructor called" << endl;
-    }
-
-    void display() {
-        cout << "Roll number:" << rollNo << endl << "Age:" << age << endl;
+    void display(){
+        cout<<"Roll number:"<<rollNo<<endl<<"Age:"<<age<<endl;
     }
 };
 
-class Child1 : public Parent1, public Parent2 {
-public:
-    Child1() : Parent1(), Parent2() {
+class Child1: public Parent1, public Parent2{
+    public:
+    Child1(): Parent1(), Parent2(){
         Parent1::display();
         Parent2::display();
         getchar();
     }
-
-    ~Child1() {
-        cout << "Child1 destructor called" << endl;
-    }
 };
 
-class Child2 : public Parent1 {
-public:
-    Child2() : Parent1() {
+class Child2: public Parent1{
+    public:
+    Child2(): Parent1(){
         Parent1::display();
     }
-
-    ~Child2() {
-        cout << "Child2 destructor called" << endl;
-    }
 };
 
-int main() {
+int main(){
     Child1 C1;
     Child2 C2;
     return 0;
